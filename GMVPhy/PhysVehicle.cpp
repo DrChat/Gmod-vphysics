1dae1f2b8059de299a7a89bd09307df7aa2e5c01
DrChat <arkolbed@gmail.com>
2014-01-14 20:56:55 -0600

    Actually start using includes Adding some files I forgot to include previously.

---
+++ GMVPhy/PhysVehicle.cpp
@@ -0,0 +1,13 @@
+#include "PhysVehicle.h"
+
+#include <GarrysMod/Lua/Interface.h>
+#include <vphysics_interface.h>
+#include "../include/vphysics_interfaceV32.h"
+
+#include "MiscFuncs.h"
+
+int Init_PhysVehicle(lua_State *state) {
+
+
+	return 0;
+}
---
+++ GMVPhy/PhysVehicle.h
@@ -0,0 +1,8 @@
+#ifndef PHYSVEHICLE_H
+#define PHYSVEHICLE_H
+
+struct lua_State;
+
+int Init_PhysVehicle(lua_State *state);
+
+#endif // PHYSVEHICLE_H
