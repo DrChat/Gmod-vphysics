ebc14bf75476f720551502840ec2179e8cb0bab7
DrChat <arkolbed@gmail.com>
2014-04-03 07:17:17 -0500

    Expose "VPhysics032" interface mainly as a way to check if the game is running this version

--- src/Physics.cpp
+++ src/Physics.cpp
@@ -30,6 +30,7 @@ CPhysics::~CPhysics() {
 	// Probably not the place we should be doing this, but who cares.
 	// This'll be called when vphysics is unloaded, and since we're (most likely) the only module with
 	// memory debugging enabled, memory leaks should only correspond to our code.
+	// Also convars "leak" 2 bytes of memory because this is being called before it should
 	_CrtDumpMemoryLeaks();
 #endif
 }
@@ -90,7 +91,7 @@ IPhysicsCollisionSet *CPhysics::FindOrCreateCollisionSet(unsigned int id, int ma
 		return m_collisionSets[id];
 
 	CPhysicsCollisionSet *set = NULL;
-	if (maxElementCount < sizeof(int) * 8) {
+	if (maxElementCount < sizeof(int) * 8) { // Limit of 32 because of the way this works internally
 		set = ::CreateCollisionSet(maxElementCount);
 		m_collisionSets.AddToTail(set);
 	}
@@ -114,3 +115,4 @@ void CPhysics::DestroyAllCollisionSets() {
 
 CPhysics g_Physics;
 EXPOSE_SINGLE_INTERFACE_GLOBALVAR(CPhysics, IPhysics, VPHYSICS_INTERFACE_VERSION, g_Physics);
+EXPOSE_SINGLE_INTERFACE_GLOBALVAR(CPhysics, IPhysics32, "VPhysics032", g_Physics); // "Undocumented" way to determine if this is the newer vphysics or not.
