a55a1b34fd195b8be0898886021f0deccbf3ebcd
DrChat <arkolbed@gmail.com>
2014-04-10 07:37:43 -0500

    Turn off the unused bullet profiler

--- bullet/src/LinearMath/btQuickprof.h
+++ bullet/src/LinearMath/btQuickprof.h
@@ -16,7 +16,8 @@
 #define BT_QUICK_PROF_H
 
 // To disable built-in profiling, please comment out next line
-//#define BT_NO_PROFILE 1
+// DrChat: Disabled because this is useless to us
+#define BT_NO_PROFILE 1
 #ifndef BT_NO_PROFILE
 #include <stdio.h>// @todo remove this, backwards compatibility
 #include "btScalar.h"
