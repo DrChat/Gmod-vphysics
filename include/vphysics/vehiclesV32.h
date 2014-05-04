386ee20254e379b5673302853f1ef2d0530b0ad5
DrChat <arkolbed@gmail.com>
2014-05-03 21:04:29 -0500

    Whoops! Remove UpdateVehicle API from the interface too.

--- include/vphysics/vehiclesV32.h
+++ include/vphysics/vehiclesV32.h
@@ -10,11 +10,6 @@
 
 class IPhysicsVehicleController32 : public IPhysicsVehicleController {
 	public:
-		// Updates the internal vehicle without calculating anything related to controls.
-		// Use this instead of Update if you want to manually control handling.
-		// Also, you should be calling UpdateVehicle after you call all of the SetX functions
-		virtual void					UpdateVehicle(float dt) = 0;
-
 		// force in kg*in/s
 		virtual void					SetWheelForce(int wheelIndex, float force) = 0;
 		virtual void					SetWheelBrake(int wheelIndex, float brakeVal) = 0;
