ea2901661fab077507fb869076464ba7ae4a4e37
DrChat <arkolbed@gmail.com>
2015-02-24 08:13:32 -0600

    New Buoyancy (WIP)

--- src/Physics_FluidController.cpp
+++ src/Physics_FluidController.cpp
@@ -10,52 +10,6 @@
 // memdbgon must be the last include file in a .cpp file!!!
 #include "tier0/memdbgon.h"
 
-static void CalculateConvexBuoyancyForce(btConvexShape *pConvex, btTransform &objRelTrans, Vector4D fluidSurface, float fluidDensity, float convexBuoyancyRatio, btVector3 &force, btVector3 &torque) {
-	static const btVector3 parallelDirections[4] = {
-		btVector3( 1, 0, 0),
-		btVector3(-1, 0, 0),
-		btVector3( 0, 0, 1),
-		btVector3( 0, 0,-1),
-	};
-	static const btVector3 up(0, 1, 0);
-
-	btVector3 norm;
-	ConvertDirectionToBull(fluidSurface.AsVector3D(), norm);
-	for (int i = 0; i < 4; i++) {
-
-	}
-}
-
-static void CalculateCompoundBuoyancyForce(btCompoundShape *pCompound, Vector4D fluidSurface, btScalar fluidDensity, btScalar ratio, btVector3 &force, btVector3 &torque) {
-	
-	
-	for (int i = 0; i < pCompound->getNumChildShapes(); i++) {
-		
-	}
-}
-
-// TODO: Buoyancy calculation
-static void CalculateBuoyancyForce(CPhysicsObject *pObject, Vector4D fluidSurface, float fluidDensity) {
-	btRigidBody *pBody = pObject->GetObject();
-	btCollisionShape *pShape = pBody->getCollisionShape();
-
-	if (pShape->isCompound()) {
-		btCompoundShape *pCompound = (btCompoundShape *)pShape;
-		btVector3 force, torque;
-		CalculateCompoundBuoyancyForce(pCompound, fluidSurface, fluidDensity, pObject->GetBuoyancyRatio(), force, torque);
-
-		pBody->applyCentralForce(force);
-		pBody->applyTorque(torque);
-	} else if (pShape->isConvex()) {
-		btConvexShape *pConvex = (btConvexShape *)pShape;
-		btVector3 force, torque;
-		CalculateConvexBuoyancyForce(pConvex, pBody->getWorldTransform(), fluidSurface, fluidDensity, pObject->GetBuoyancyRatio(), force, torque);
-
-		pBody->applyCentralForce(force);
-		pBody->applyTorque(torque);
-	}
-}
-
 /********************************
 * CLASS CPhysicsFluidCallback
 ********************************/
@@ -165,8 +119,58 @@ int	CPhysicsFluidController::GetContents() const {
 	return m_iContents;
 }
 
+static btVector3 calcConvexCenter(btConvexHullShape *pShape, btVector3 &planePos, btVector3 &planeNorm) {
+	// Basic average
+	btVector3 sum(0, 0, 0);
+
+	if (pShape->getNumPoints() > 0) {
+		for (int i = 0; i < pShape->getNumPoints(); i++) {
+			btVector3 point = pShape->getScaledPoint(i);
+			point -= planePos;
+
+			// Only add the point if it's submerged
+			if (point.dot(planeNorm) < 0)
+				sum += pShape->getScaledPoint(i);
+		}
+
+		sum /= pShape->getNumPoints();
+	}
+
+	return sum;
+}
+
+// Find the object's center of buoyancy
+// This would be the center of all submerged points
+// You can bisect the object by the plane of the water surface to get all points
+// submerged, then calculate the center of all those points
+// planePos: World space plane position
+// planeNorm: World space plane normal
+static btVector3 calculateBuoyantCenter(btRigidBody *pBody, btVector3 &planePos, btVector3 &planeNorm) {
+	btVector3 center(0, 0, 0);
+
+	btVector3 relPlanePos = pBody->getWorldTransform().inverse() * planePos;
+	btVector3 relNorm = quatRotate(pBody->getWorldTransform().getRotation().inverse(), planeNorm);
+
+	btCollisionShape *pShape = pBody->getCollisionShape();
+	if (pShape->getShapeType() == COMPOUND_SHAPE_PROXYTYPE) {
+		btCompoundShape *pCompound = (btCompoundShape *)pShape;
+		for (int i = 0; i < pCompound->getNumChildShapes(); i++) {
+			btCollisionShape *pChild = pCompound->getChildShape(i);
+			if (pChild->getShapeType() == CONVEX_HULL_SHAPE_PROXYTYPE) {
+				center += calcConvexCenter((btConvexHullShape *)pChild, relPlanePos, relNorm);
+			}
+		}
+
+		if (pCompound->getNumChildShapes() > 0)
+			center /= pCompound->getNumChildShapes();
+	}
+
+	return center;
+}
+
 // TODO: Refactor this code to be less messy.
 void CPhysicsFluidController::Tick(float dt) {
+	// TODO: Buoyancy calculation
 	int numObjects = m_pGhostObject->getNumOverlappingObjects();
 	for (int i = 0; i < numObjects; i++) {
 		btRigidBody *body = btRigidBody::upcast(m_pGhostObject->getOverlappingObject(i));
@@ -176,6 +180,60 @@ void CPhysicsFluidController::Tick(float dt) {
 		CPhysicsObject *pObject = (CPhysicsObject *)body->getUserPointer();
 		Assert(pObject);
 
+		// Find the surface plane's world pos (center at the very top)
+		btVector3 surfPos = m_pGhostObject->getWorldTransform().getOrigin();
+		btVector3 surfNorm;
+		ConvertDirectionToBull(m_vSurfacePlane.AsVector3D(), surfNorm);
+
+		btScalar fluidLen = ConvertDistanceToBull(m_vSurfacePlane.w);
+
+		btVector3 omins, omaxs;
+		m_pGhostObject->getCollisionShape()->getAabb(m_pGhostObject->getWorldTransform(), omins, omaxs);
+		btScalar height = omaxs.y() - omins.y();
+
+		surfPos += surfNorm * (height / 2);
+
+		btVector3 center = calculateBuoyantCenter(body, surfPos, surfNorm);
+		center = body->getWorldTransform() * center; // Convert back into world space
+
+		btVector3 offset = center - surfPos;
+		btScalar submerged = surfNorm.dot(offset);
+
+		IVPhysicsDebugOverlay *pOverlay = m_pEnv->GetDebugOverlay();
+		if (pOverlay) {
+			Vector pos;
+			ConvertPosToHL(surfPos, pos);
+			pOverlay->AddBoxOverlay(pos, Vector(-8), Vector(8), QAngle(0, 0, 0), 255, 0, 0, 255, 0.f);
+			pOverlay->AddLineOverlay(pos, pos + m_vSurfacePlane.AsVector3D() * 32, 255, 0, 0, false, 0.f);
+
+			ConvertPosToHL(center, pos);
+			pOverlay->AddBoxOverlay(pos, Vector(-8), Vector(8), QAngle(0, 0, 0), 0, 0, 255, 255, 0.f);
+
+			if (submerged < 0) {
+				pOverlay->AddTextOverlay(pos, 0.f, "submerged %+04.2f", submerged);
+			}
+		}
+
+		if (submerged < 0) {
+			btVector3 mins, maxs;
+			body->getAabb(mins, maxs);
+
+			// Calc the volume coefficient
+			btScalar dist = -submerged;
+			float p = clamp(dist / 1.2, 0.f, 1.f);
+			btScalar vol = p * pObject->GetVolume();
+
+			// TODO: Need a way to calculate this properly
+			// Force should be exactly equal to -gravity when submerged distance is 0
+			// density units kg/m^3
+			btVector3 force = (m_fDensity * -body->getGravity() * vol) * pObject->GetBuoyancyRatio();
+
+			btVector3 relPos = center - body->getWorldTransform().getOrigin();
+			body->applyForce(force, relPos);
+		}
+
+		// Old code that actually works better
+		/*
 		btVector3 mins, maxs, omins, omaxs;
 		body->getAabb(mins, maxs);
 		m_pGhostObject->getCollisionShape()->getAabb(m_pGhostObject->getWorldTransform(), omins, omaxs);
@@ -201,6 +259,7 @@ void CPhysicsFluidController::Tick(float dt) {
 		// FIXME: Damping would be way too much for an object only partially touching our surface (ex. giant fucking bridge with like 1 pixel in the water)
 		body->setLinearVelocity(body->getLinearVelocity() * (1.0f - (0.75f * dt)));
 		body->setAngularVelocity(body->getAngularVelocity() * (1.0f - (0.75f * dt)));
+		*/
 	}
 }
 
